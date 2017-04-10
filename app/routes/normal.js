d8f422e4d49a5c82dedd48a52a4bc6ca6ca00f63
sirCrusader <sercrusader@gmail.com>
2017-04-10 20:39:19 +0300

    Removed unused route.

--- app/routes/normal.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res) {
-  res.render('normal', { title: 'Example', text: 'This is example of the custom route' });
-});
-
-module.exports = router;
