763bbf0308b9af4a71114c2e0acda524a352d3a6
sirCrusader <sercrusader@gmail.com>
2017-04-13 15:14:35 +0300

    Added new controllers and actions.

--- app/app_server/controllers/locations.js
+++ app/app_server/controllers/locations.js
@@ -1,3 +1,15 @@
 /**
  * Created by wizard on 4/13/17.
  */
+
+module.exports.homeList = function (req, res) {
+    res.render('index', { title: 'Home' });
+};
+
+module.exports.locationInfo = function (req, res) {
+    res.render('index', { title: 'Location Info' });
+};
+
+module.exports.addReview = function (req, res) {
+    res.render('index', { title: 'Add review' });
+};
--- app/app_server/controllers/others.js
+++ app/app_server/controllers/others.js
@@ -1,3 +1,7 @@
 /**
  * Created by wizard on 4/13/17.
  */
+
+module.exports.about = function (req, res) {
+    res.render('about', { title: 'About' });
+};
