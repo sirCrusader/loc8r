c2acd2aacf8629d9b78e8013b174f4783ea32f0f
sirCrusader <sercrusader@gmail.com>
2017-04-13 10:13:38 +0300

    Added new routes.

--- app/app_server/routes/index.js
+++ app/app_server/routes/index.js
@@ -3,7 +3,4 @@ var router = express.Router();
 var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
-
 module.exports = router;
