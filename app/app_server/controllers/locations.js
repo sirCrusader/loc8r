d1e2c742caff888dea3de61b1a75b62234b99a2d
sirCrusader <sercrusader@gmail.com>
2017-04-17 08:27:50 +0300

    Added static html for review form page.

--- app/app_server/controllers/locations.js
+++ app/app_server/controllers/locations.js
@@ -11,5 +11,5 @@ module.exports.locationInfo = function (req, res) {
 };
 
 module.exports.addReview = function (req, res) {
-    res.render('index', { title: 'Add review' });
+    res.render('location-review-form', { title: 'Add review' });
 };
