ec2989ef375629a1375d7489ea50c5500a56b1ae
sirCrusader <sercrusader@gmail.com>
2017-05-01 20:44:10 +0300

    Added validation to the review form.

--- app/app_server/controllers/locations.js
+++ app/app_server/controllers/locations.js
@@ -77,7 +77,8 @@ var renderReviewForm = function (req, res, locDetail) {
         title: 'Review ' + locDetail.name + ' on Loc8r',
         pageHeader: {
             title: 'Review ' + locDetail.name,
-        }
+        },
+        error: req.query.err,
     });
 };
 
@@ -168,14 +169,20 @@ module.exports.doAddReview = function(req, res) {
         method: "POST",
         json: postdata
     };
-    request(
-        requestOptions,
-        function (err, response, body) {
-            if (response.statusCode === 201) {
-                res.redirect('/location/' + locationid);
-            } else {
-                _showError(req, res, response.statusCode)
+    if (!postdata.author || !postdata.rating || !postdata.reviewText) {
+        res.redirect('/location/' + locationid + '/review/new?err=val');
+    } else {
+        request(
+            requestOptions,
+            function (err, response, body) {
+                if (response.statusCode === 201) {
+                    res.redirect('/location/' + locationid);
+                } else if (response.statusCode === 400 && body.name && body.name === "ValidationError" ) {
+                    res.redirect('/location/' + locationid + '/reviews/new?err=val');
+                } else {
+                    _showError(req, res, response.statusCode)
+                }
             }
-        }
-    )
+        )
+    }
 };
---
+++ app/public/javascripts/validation.js
@@ -0,0 +1,15 @@
+/**
+ * Created by wizard on 5/1/17.
+ */
+
+$('#addReview').submit(function (e) {
+    $('.alert.alert-danger').hide();
+    if (!$('input#name').val() || !$('select#rating').val() || !$('textarea#review').val()) {
+        if ($('.alert.alert-danger').length) {
+            $('.alert.alert-danger').show();
+        } else {
+            $(this).prepend('<div role="alert" class="alert alert-danger">All fields required, please try again</div>')
+        }
+        return false;
+    }
+});
