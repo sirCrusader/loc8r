d13926e29966f982b4d0dff781152198236736d3
sirCrusader <sercrusader@gmail.com>
2017-05-02 20:06:11 +0300

    Added stars for every location.

--- app/public/javascripts/angular/loc8rApp.js
+++ app/public/javascripts/angular/loc8rApp.js
@@ -1,6 +1,36 @@
 /**
  * Created by wizard on 5/2/17.
  */
+var _isNumeric = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+var formatDistance = function() {
+    return function (distance) {
+        var numDistance, unit;
+        if (distance && _isNumeric(distance)) {
+            if (distance > 1) {
+                numDistance = parseFloat(distance).toFixed(1);
+                unit = 'km';
+            } else {
+                numDistance = parseInt(distance * 1000,10);
+                unit = 'm';
+            }
+            return numDistance + unit;
+        } else {
+            return "?";
+        }
+    }
+};
+
+var ratingStars = function() {
+    return {
+        scope: {
+            thisRating: '=rating'
+        },
+        templateUrl: '/angular/rating-stars.html'
+    };
+};
 
 var locationListCtrl = function ($scope) {
     $scope.data = {
@@ -24,4 +54,6 @@ var locationListCtrl = function ($scope) {
 
 angular
     .module('loc8rApp', [])
-    .controller('locationListCtrl', locationListCtrl);
+    .controller('locationListCtrl', locationListCtrl)
+    .filter('formatDistance', formatDistance)
+    .directive('ratingStars', ratingStars);
