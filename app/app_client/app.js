baa95879e4dd1ee0e1dd54fa23bc2f7530cf9531
sirCrusader <sercrusader@gmail.com>
2017-05-08 21:23:22 +0300

    turn on html5mode for urls

--- app/app_client/app.js
+++ app/app_client/app.js
@@ -7,17 +7,19 @@
         'ngRoute',
     ]);
 
-    function config ($routeProvider) {
+    function config ($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'home/home.view.html',
                 controller: 'homeCtrl',
                 controllerAs: 'vm'
             })
-            .otherwise({ redirectTo: '/' })
+            .otherwise({ redirectTo: '/' });
+
+        $locationProvider.html5Mode(true);
     }
 
     angular
         .module('loc8rApp')
-        .config(['$routeProvider', config]);
+        .config(['$routeProvider', '$locationProvider', config]);
 })();
